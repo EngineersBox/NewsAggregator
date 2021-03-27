915eb29ec9c9d25ce967f6db9df4918a1283c0ef
u6940136@anu.edu.au <toastcraft.info@gmail.com>
2021-03-28 02:14:32 +1100

    :hammer: Merged in main and applied local stashed changes

--- modules/RateLimiter/src/config.py
+++ modules/RateLimiter/src/config.py
@@ -1,11 +1,14 @@
 CONNECTION = {
     "host_address": "127.0.0.1", # Redis DB IPv4 address
-    "port": 1234, # Redis DB port
+    "port": 6379,  # Redis DB port
     "db": 0 # Redis DB ID
 }
 LIMITING = {
     "key_prefix": "ratelimit:", # Prefix for rate limiter keys
     # TODO: Find an acceptable value for the max_reqs based on use cases
-    "max_reqs": 30, # Maximum number of requests in the expiry period
-    "expiry": 1 # Expiry period for an IP address in seconds
+    "max_reqs": 2, # Maximum number of requests in the expiry period
+    "expiry": 1000  # Expiry period for an IP address in milliseconds (E.g. 30000ms = 30s)
+}
+LUA_INCR_TTL = {
+    "location": "resources/ratelimiter/incr_ttl.lua" # Relative location of the lua script
 }
--- modules/RateLimiter/src/request_handler.py
+++ modules/RateLimiter/src/request_handler.py
@@ -1,6 +1,8 @@
-import functools
+import functools, socket
 from redis import Redis, ConnectionPool
-from .config import LIMITING, CONNECTION
+from .lua_script_result import LuaScriptResult
+from .invoker import Invoker, InvocationSchema
+from .config import LIMITING, CONNECTION, LUA_INCR_TTL
 from .exceptions import HTTP_429_TooManyRequests, HTTP_400_BadRequest
 from flask import request, jsonify
 
@@ -9,15 +11,6 @@ REDIS_CONNECTION_CONFIG = ConnectionPool(
     port=CONNECTION["port"],
     db=CONNECTION["db"]
 )
-
-def keyFormat(ip_addr: str) -> str:
-    '''
-    Formats a redis key with the prefix from config
-
-    # Arguments:
-    - ip_addr <str> :: IP address of the request (E.g. "104.25.94.32")
-    '''
-    return LIMITING["key_prefix"] + "{0}".format(ip_addr)
 
 class RateLimiter:
 
@@ -36,6 +29,13 @@ class RateLimiter:
         self.max_reqs = max_reqs
         self.expiry = expiry
         self.redis = Redis(connection_pool=REDIS_CONNECTION_CONFIG)
+        self.script = RateLimiter.getLuaScript()
+        # Create a schema for the LuaScriptResult constructor arguments:
+        # LuaScriptResult.__init__(reqcount: int, timeleft: int)
+        self.script_result_schema = [
+            InvocationSchema("reqcount", int),
+            InvocationSchema("timeleft", int)
+        ]
 
     def __call__(self, func):
         '''
@@ -51,16 +51,16 @@ class RateLimiter:
             try:
                 with self:
                     return func(*args, **kwargs)
-            except HTTP_429_TooManyRequests:
+            except HTTP_429_TooManyRequests as e:
                 response = {
                     'status_code': 429,
-                    'status': 'You have exceeded the rate limit of {0} requests per {1} seconds'.format(LIMITING["max_reqs"], LIMITING["expiry"])
+                    'status': 'You have exceeded the rate limit of {0} requests per {1} milliseconds. Try again in {2} ms'.format(LIMITING["max_reqs"], LIMITING["expiry"], e.expiry)
                 }
                 return jsonify(response), 429
-            except HTTP_400_BadRequest:
+            except HTTP_400_BadRequest as e:
                 response = {
                     'status_code': 400,
-                    'status': 'Request not formatted correctly'
+                    'status': e.message
                 }
                 return jsonify(response), 400
             except:
@@ -77,18 +77,60 @@ class RateLimiter:
     def __exit__(self):
         return
 
+    @staticmethod
+    def keyFormat(ip_addr: str) -> str:
+        '''
+        Formats a redis key with the prefix from config
+
+        # Arguments:
+        - ip_addr <str> :: IP address of the request (E.g. "104.25.94.32")
+
+        # Throws
+        - HTTP_440_BadRequest <Exception> :: If the client address is not valid a 400 is returned
+
+        # Returns
+        - <str> :: A formatted key in the format "<PREFIX>:<IP ADDRESS>"
+        '''
+        try:
+            # Check if the address is valid under relevant RFC standards
+            socket.inet_aton(ip_addr)
+        except socket.error:
+            # If the address is invalid return an HTTP 400 with the address in the message
+            raise HTTP_400_BadRequest("Invalid client address: {0}".format(ip_addr))
+        # Format the DB key as  "<PREFIX>:<IP ADDRESS>"
+        return LIMITING["key_prefix"] + ip_addr
+    
+    @staticmethod
+    def getLuaScript() -> str:
+        with open(LUA_INCR_TTL["location"], "r") as lua_script:
+            # Return the entire Lua script as a string
+            return lua_script.read()
+
+    def handleRedisResult(self, result: LuaScriptResult):
+        # Check if the request has exceeded the maximum in this time frame
+        if (result.reqcount > self.max_reqs):
+            # If the request count has exceeded the max, return an HTTP 429 with the time left
+            raise HTTP_429_TooManyRequests(result.timeleft)
+        return 200
+
     def incrementRate(self) -> int:
         '''
         If the current request count is greater than the max requests then raise
         a HTTP_429_TooManyRequests exception. Otherwise do nothing and return the
         current request count
         '''
-        # TODO: To be completed as part of REDIS-30
-        pass
-
-    def getExpiryTime(self) -> int:
-        '''
-        Get an approximation of the duration left for a given IP address
-        '''
-        # TODO: In subsequent ticket
-        pass
+        # Execute the Lua script with key, max_reqs and expiry arguments
+        result = self.redis.eval(
+            self.script,
+            1, # Number of keys before arguments
+            self.keyFormat(self.client_addr), # KEYS[1]
+            self.max_reqs, # ARGS[1]
+            self.expiry # ARGS[2]
+        )
+        # Create an invoker instance to handle generic argument passing to a LuaScriptResult instance
+        invoker = Invoker(LuaScriptResult, self.script_result_schema)
+        # Invoke an instance of LuaScriptResult with the results from the script (list)
+        lua_result =  invoker.reflectInvoke(result)
+        print(lua_result)
+        # Handle the results accordingly
+        return self.handleRedisResult(lua_result)
