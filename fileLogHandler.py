453f7b3372cadf320590945d20ac242813e30549
u6940136@anu.edu.au <toastcraft.info@gmail.com>
2021-04-20 18:10:21 +1000

    Created logger for flask applet

--- fileLogHandler.py
+++ fileLogHandler.py
@@ -2,6 +2,6 @@ import logging, datetime, os
 
 class FileHandler(logging.FileHandler):
     def __init__(self, path, mode):
-        path = path + "/" + datetime.datetime.now().isoformat()
+        path = "{}/{}.log".format(path, datetime.datetime.now().isoformat())
         os.mkdir(path)
         super(FileHandler, self).__init__(path, mode)
