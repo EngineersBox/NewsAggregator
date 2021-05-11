ead063665e0c0fa8192c03d5643663e1c7bb7c84
mightybeast <u6123487@anu.edu.au>
2021-05-11 22:23:28 +0800

    :construction: Update cuckoo filter delete function according to pseudocode.

--- modules/CuckooFilter/src/command/delete.rs
+++ modules/CuckooFilter/src/command/delete.rs
@@ -8,13 +8,17 @@ use resolver::resolver::Resolver;
 use command::error_handler::redis_command_error_handler;
 // macros
 use crate::{command_entry_debug_log, handled_templated_error, push_all, validate_schema};
+// hash
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 
-/* DELETE command has one argument: query */
+/* DELETE command has 2 argument: hash, fingerprint */
 fn construct_arguments_schema() -> Vec<Argument> {
     let mut arg_schema: Vec<Argument> = Vec::new();
     push_all!(
         arg_schema,
-        Argument::new("query", ArgType::STRING)
+        Argument::new("hash", ArgType::STRING)
+        Argument::new("fingerprint", ArgType::STRING)
     );
     return arg_schema;
 }
@@ -34,19 +38,43 @@ pub fn cuckoofilter_delete(ctx: &Context, args: Vec<String>) -> RedisResult {
         resolver, ctx,
         "Could not retrieve arguments"
     );
+    
+    // &args[0] is command "cf.delete"
+    let hash = &args[1]; // hash is i1 in pseudocode
+    let fingerprint = &args[2];
 
-    let key = &args[1]; // &args[0] is command "cf.delete"
-    let mut cuckoo_filter = cuckoofilter::CuckooFilter::new();
-
-    // remove query from Redis, non-existing key is ignored
-    let remove_query = ctx.call("ZREM", &["query-frequency", &key]);
-    match remove_query {
-        Ok(_) => ctx.log_verbose("Successfully remove query"),
-        Err(_) => ctx.log_verbose("Error removing query"),
+    let mut exist = ctx.call("SISMEMBER", &[&hash, &fingerprint]).unwrap();
+    let mut exist_int: u64 = RedisValue::into(exist); // can only be 1 or 0
+    if exist_int == 1 {
+        ctx.log_verbose("Query is in cache.");
+        return ctx.call("SREM", &[&hash, &fingerprint])
+    } else {
+        // calculate i2
+        let i1 = hash.parse::<u64>().unwrap();
+        let mut h = DefaultHasher::new();
+        fingerprint.hash(&mut h);
+        let i2 = (i1 ^ h.finish()).to_string();
+        exist = ctx.call("SISMEMBER", &[&i2, &fingerprint]).unwrap();
+        exist_int: u64 = RedisValue::into(exist);
+        if exist_int == 1 {
+            ctx.log_verbose("Query is in cache.");
+            return ctx.call("SREM", &[&i2, &fingerprint])
+        } else {
+            ctx.log_verbose("Query is not in cache.");
+        }
     }
+    return RedisResult::Ok(RedisValue::from("Nothing to delete."))
+
+    // let mut cuckoo_filter = cuckoofilter::CuckooFilter::new();
+    // remove query from Redis, non-existing key is ignored
+    // let remove_query = ctx.call("ZREM", &["query-frequency", &h]);
+    // match remove_query {
+    //     Ok(_) => ctx.log_verbose("Successfully remove query"),
+    //     Err(_) => ctx.log_verbose("Error removing query"),
+    // }
     // remove query from cuckoo filter, non-existing key is ignored
-    let key_exists = cuckoo_filter.delete(&[&key]);
+    // let key_exists = cuckoo_filter.delete(&[&hash]);
 
     // return false if key does not exist in filter
-    return RedisResult::Ok(RedisValue::from(key_exists.to_string()))
+    // return RedisResult::Ok(RedisValue::from(key_exists.to_string()))
 }
