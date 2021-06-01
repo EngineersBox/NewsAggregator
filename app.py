6a060b3c554d6275700df9e75f06a803405931f1
u6940136@anu.edu.au <toastcraft.info@gmail.com>
2021-06-01 16:46:05 +1000

    :hammer: Moved SpaCy and knn changes to seperate branch

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template, send_from_directory
 from flask_cors import CORS, cross_origin
 from minitask.simple_search import simple_match_search
 from elasticsearch import Elasticsearch
-from knn_indexing.summary_1.summary import body_summary
+from summary_1.summary import body_summary
 from knn_indexing.index import knn_query
 from modules.RateLimiter.src.request_handler import RateLimiter
 from urllib3.exceptions import SSLError
