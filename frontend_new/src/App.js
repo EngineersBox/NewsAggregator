c016a78fa7eafca8957948e60003a523a2573d61
jay <u6044681@anu.edu.au>
2021-08-24 00:10:42 +1000

    Implement Search TextField and Buttons

--- frontend_new/src/App.js
+++ frontend_new/src/App.js
@@ -1,12 +1,25 @@
-import './App.css';
-import TopBar from './TopBar';
 import { ThemeProvider } from '@material-ui/core/styles'
 import { theme } from './theme'
+import './App.css';
+import TopBar from './TopBar';
+import Search from './Search';
+import Grid from '@material-ui/core/Grid';
 
+import Grow from '@material-ui/core/Grow';
 function App() {
   return (
 	     <ThemeProvider theme={theme}>
+	  <Grow in={true} timeout={1600}>
+	<Grid container justifyContent="center" alignItems="stretch" direction="row" spacing={3} >
+		<Grid item xs={12}>
 	  <TopBar/>
+		  </Grid>
+		<Grid item xs={12}>
+			
+		  <Search/>
+			  </Grid >
+			  </Grid >
+			  </Grow>
 		  </ThemeProvider>
   );
 }
---
+++ frontend_new/src/Search.js
@@ -0,0 +1,51 @@
+import './App.css';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    input: {
+      margin: theme.spacing(1),
+      height: 60 
+    }
+  })
+);
+function Search(props) {
+const classes = useStyles();
+function progress(){
+props.submit(true);
+}
+  return (
+	<Grid container justifyContent="center" direction="row" spacing={1} >
+		<Grid item xs={11} lg={6}>
+			<TextField
+			    id="search-input"
+			    variant="outlined"
+			    color="secondary"
+				fullWidth
+				  InputProps={{
+			  className: classes.input
+			}}
+			  />
+			  </Grid>
+		<Grid item xs={5} lg={2}  >
+			    <Button className={classes.input} variant="contained" color="secondary"  fullWidth>
+				    Accurate Search
+      </Button>
+			  </Grid>
+		<Grid item xs={5} lg={2}  >
+			    <Button className={classes.input}  variant="contained"  color="secondary"  fullWidth>
+				    Associate Search
+      </Button>
+			  </Grid>
+				  </Grid>
+
+  );
+}
+
+export default Search;
