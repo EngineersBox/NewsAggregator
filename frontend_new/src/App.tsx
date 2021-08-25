0567a4c4c13a068c711cf6d8067bede109b62b0c
jay <u6044681@anu.edu.au>
2021-08-25 15:36:58 +1000

    add info buttons to search buttons

--- frontend_new/src/App.tsx
+++ frontend_new/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import TopBar from './TopBar';
 import Search from './Search';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
 import Grow from '@material-ui/core/Grow';
 function App() {
@@ -13,6 +14,18 @@ function App() {
 	<Grid container justifyContent="center" alignItems="stretch" direction="row" spacing={3} >
 		<Grid item xs={12}>
 	  <TopBar/>
+		  </Grid>
+		<Grid item xs={12}>
+		  </Grid>
+		<Grid item xs={12}>
+		  <Typography align='center' variant="h4" component="h2" gutterBottom>
+       Using Open Source Technologies to Create a Search Engine 
+      </Typography>
+		  </Grid>
+		<Grid item xs={12}>
+		  <Typography align='center' variant="h5" component="h2" gutterBottom>
+		  NewsAggregator used Elastic Search, Redis and Rust in the backend, and React and Material UI for the frontend.
+      </Typography>
 		  </Grid>
 		<Grid item xs={12}>
 			
--- frontend_new/src/Search.tsx
+++ frontend_new/src/Search.tsx
@@ -3,6 +3,7 @@ import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import InfoButton from './InfoButton';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -28,15 +29,29 @@ const classes = useStyles();
 			  />
 			  </Grid>
 		<Grid item xs={5} lg={2}  >
-			    <Button className={classes.input} variant="contained" color="secondary"  fullWidth>
+	<Grid container justifyContent="center" direction="row" spacing={1} >
+		<Grid item xs={11}  >
+			    <Button className={classes.input} variant="contained" color="secondary" fullWidth>
 				    Accurate Search
       </Button>
+      </Grid>
+		<Grid item xs={1}  >
+      <InfoButton/>
+      </Grid>
+      </Grid>
 			  </Grid>
 		<Grid item xs={5} lg={2}  >
+	<Grid container justifyContent="center" direction="row" spacing={1} >
+		<Grid item xs={11}  >
 			    <Button className={classes.input}  variant="contained"  color="secondary"  fullWidth>
 				    Associate Search
       </Button>
+      </Grid>
+		<Grid item xs={1}  >
+      <InfoButton/>
 			  </Grid>
+				  </Grid>
+				  </Grid>
 				  </Grid>
 
   );
