5a1388c26291300f7d83a3437b115396833894ea
Sharon Huang <xiyangyang2000@hotmail.com>
2021-09-20 20:29:09 +1000

    merge

--- frontend_new/src/components/SlideAlert.tsx
+++ frontend_new/src/components/SlideAlert.tsx
@@ -9,6 +9,8 @@ import Slide from "@material-ui/core/Slide";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 import IconButton from "@material-ui/core/IconButton";
 import { TransitionProps } from "@material-ui/core/transitions";
+import { light } from "../themes/light";
+import { dark } from "../themes/dark";
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & { children?: React.ReactElement<any, any> },
@@ -19,6 +21,7 @@ const Transition = React.forwardRef(function Transition(
 
 type props = {
   text: string;
+  whichTheme:boolean;
 };
 
 
@@ -47,12 +50,24 @@ export default function AlertDialogSlide(props: props) {
         onClose={handleClose}
         aria-labelledby="alert-dialog-slide-title"
         aria-describedby="alert-dialog-slide-description"
-        PaperProps={{
-          style: {
-              color:"#fff",
-              backgroundColor:""
-          },
-       }}
+        PaperProps={
+          props.whichTheme
+            ? {
+                style: {
+                    color:"#fff",
+                    backgroundColor:dark.palette.background.default
+                },
+              }
+            : {
+              style: {
+                  color:"#fff",
+                  backgroundColor:light.palette.background.default
+              },
+            }
+      
+      
+      
+      }
       >
         <DialogTitle id="alert-dialog-slide-title" >{"Information" }</DialogTitle>
         <DialogContent >
