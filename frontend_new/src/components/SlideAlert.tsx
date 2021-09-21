acd66f1a6ce4c832e79f3a77e6c2ddbe74915853
Sharon Huang <xiyangyang2000@hotmail.com>
2021-09-21 19:52:58 +1000

    re-push theme for SlideAlert

--- frontend_new/src/components/SlideAlert.tsx
+++ frontend_new/src/components/SlideAlert.tsx
@@ -9,6 +9,8 @@ import Slide from "@material-ui/core/Slide";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 import IconButton from "@material-ui/core/IconButton";
 import { TransitionProps } from "@material-ui/core/transitions";
+import { light } from "../themes/light";
+import { dark } from "../themes/dark";
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & { children?: React.ReactElement<any, any> },
@@ -16,11 +18,15 @@ const Transition = React.forwardRef(function Transition(
 ) {
   return <Slide direction="left" ref={ref} {...props} />;
 });
+
 type props = {
   text: string;
+  whichTheme: boolean;
 };
 
+
 export default function AlertDialogSlide(props: props) {
+
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -40,20 +46,39 @@ export default function AlertDialogSlide(props: props) {
         open={open}
         TransitionComponent={Transition}
         keepMounted
+        color="secondary"
         onClose={handleClose}
         aria-labelledby="alert-dialog-slide-title"
         aria-describedby="alert-dialog-slide-description"
+        PaperProps={
+                  props.whichTheme
+                    ? {
+                        style: {
+                            color:dark.palette.secondary.main,
+                            backgroundColor:dark.palette.background.default
+                        },
+                      }
+                    : {
+                      style: {
+                          color:light.palette.secondary.main,
+                          backgroundColor:light.palette.background.default
+                      },
+                    }
+              
+              }
+
       >
-        <DialogTitle id="alert-dialog-slide-title">{"Information"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
+        <DialogTitle id="alert-dialog-slide-title" >{"Information" }</DialogTitle>
+        <DialogContent >
+          <DialogContentText id="alert-dialog-slide-description" color="secondary">
             NewsAggregator used Elastic Search, Redis and Rust in the backend,
             and React and Material UI for the frontend.
           </DialogContentText>
-          <DialogContentText id="alert-dialog-slide-description">
+          <DialogContentText id="alert-dialog-slide-description" color="secondary">
             Accurate Search: Search via a basic query matching keyword
           </DialogContentText>
-          <DialogContentText id="alert-dialog-slide-description">
+          <DialogContentText id="alert-dialog-slide-description"
+            color="secondary">
             Associative Search: Search using a classification model, KNN
           </DialogContentText>
         </DialogContent>
