39bcd95767ed5fb37fb7d4766634ef7b610d29e5
Sharon Huang <xiyangyang2000@hotmail.com>
2021-09-27 21:52:25 +1000

    new change based on Jack's comment

--- frontend_new/src/components/ThemeSetting.tsx
+++ frontend_new/src/components/ThemeSetting.tsx
@@ -1,16 +1,7 @@
 
 function getDefaultTheme() {
-    //get the current date
-    var date = new Date();
-    
-    //0 to 23
-    var hour =  date.getHours();
-
-    if (hour < 6 || hour >22) { 
-        return false; 
-    } else {
-        return true; 
-    }
+    let hour = new Date().getHours();
+    return hour < 6 || hour > 22;   
 };
 
 export default getDefaultTheme;
