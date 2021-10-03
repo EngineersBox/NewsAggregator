1e3ae103a84f650e983465e9613be803272e6bba
Mia Ravuri <u6961332@anu.edu.au>
2021-10-03 20:25:47 +1300

    Modified background colour

--- frontend_new/src/themes/light.tsx
+++ frontend_new/src/themes/light.tsx
@@ -1,16 +1,27 @@
+import { blue } from "@material-ui/core/colors";
 import { createMuiTheme } from "@material-ui/core/styles";
+
 
 export const light = createMuiTheme({
   palette: {
     primary: {
+      // Colour of bar
       main: "#f0f0f0",
     },
     secondary: {
+      //Buttons
       main: "#212121",
+      
     },
+    // tertiary: {
+    //   main: "#52ab98",
+    // }
     background: {
-      default: "#dfdfdf",
-      paper: "#bfbfbf",
+      //default: "#dfdfdf",
+      default: "#f2f2f2",
+      //paper: "#bfbfbf",
+      paper: "#ffffff",
+      
     },
   },
 });
