e17ad1ec6ea1309eaf0f5a4eee2ab3541974fbfd
Mia Ravuri <u6961332@anu.edu.au>
2021-10-03 20:48:38 +1300

    Removed comments

--- frontend_new/src/themes/light.tsx
+++ frontend_new/src/themes/light.tsx
@@ -5,21 +5,14 @@ import { createMuiTheme } from "@material-ui/core/styles";
 export const light = createMuiTheme({
   palette: {
     primary: {
-      // Colour of bar
-      main: "#f0f0f0",
+     main: "#f0f0f0",
     },
     secondary: {
-      //Buttons
       main: "#212121",
       
     },
-    // tertiary: {
-    //   main: "#52ab98",
-    // }
     background: {
-      //default: "#dfdfdf",
       default: "#f2f2f2",
-      //paper: "#bfbfbf",
       paper: "#ffffff",
       
     },
