3eed6f278940b112ff307e06da7614c92c06ba0a
Mia Ravuri <u6961332@anu.edu.au>
2021-10-08 16:13:09 +1300

    Removed unused imports frontPageInfo.tsx

--- frontend_new/src/components/FrontPageInfo.tsx
+++ frontend_new/src/components/FrontPageInfo.tsx
@@ -1,10 +1,8 @@
-import React from "react";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
 import mainLogo from './Light_Mode_Logo.png';
 import darkLogo from './Logo_Dark_Mode.png';
 import Box from '@material-ui/core/Box'
-import { ThemeProvider, useTheme } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
 
 export default function FrontPageInfo() {
   const theme = useTheme();
@@ -19,7 +17,6 @@ export default function FrontPageInfo() {
       <Grid item xs={12}></Grid>
       <Grid item xs={12}>
       <Box paddingLeft={58}> 
-      {/* // @ts-ignore: */}
       {theme.palette.type === "dark" ? <img src={darkLogo} alt="Logo"/> : <img src={mainLogo} alt="Logo"/>};
 
       </Box>
