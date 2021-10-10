b1d236f4a254d6265b0c0da5748ede7ff63278c0
Mia Ravuri <u6961332@anu.edu.au>
2021-10-10 14:46:19 +1300

    Changed spacing of logos and size

--- frontend_new/src/components/FrontPageInfo.tsx
+++ frontend_new/src/components/FrontPageInfo.tsx
@@ -3,9 +3,22 @@ import mainLogo from './Light_Mode_Logo.png';
 import darkLogo from './Logo_Dark_Mode.png';
 import Box from '@material-ui/core/Box'
 import { useTheme } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
+
+const image_size= {
+  height: 150,
+  width: 600,
+}
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    paddingLeft: theme.spacing(45), //grid padding
+    paddingBottom: theme.spacing(3), //grid padding
+  },
+}));
 
 export default function FrontPageInfo() {
   const theme = useTheme();
+  const classes = useStyles();
   return (
     <Grid
       container
@@ -16,8 +29,10 @@ export default function FrontPageInfo() {
     >
       <Grid item xs={12}></Grid>
       <Grid item xs={12}>
-      <Box paddingLeft={58}> 
-      {theme.palette.type === "dark" ? <img src={darkLogo} alt="Logo"/> : <img src={mainLogo} alt="Logo"/>};
+      <Box className={classes.paper}> 
+      {theme.palette.type === "dark" ? (<a href={"https://anu.jkl.io/"}>
+        <img style= {image_size} src={darkLogo} alt="Dark Logo"/></a>) : 
+        (<a href={"https://anu.jkl.io/"}><img style= {image_size} src={mainLogo} alt="Light Logo"/></a>)};
 
       </Box>
       </Grid>
