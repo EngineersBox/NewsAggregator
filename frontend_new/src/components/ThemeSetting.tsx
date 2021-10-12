a2f33b78bba0beb5ce6978e4b23d0188dee757a0
WeiXin Fam <wxinfam18@gmail.com>
2021-10-12 23:40:47 +1100

    dummy time test

--- frontend_new/src/components/ThemeSetting.tsx
+++ frontend_new/src/components/ThemeSetting.tsx
@@ -1,6 +1,11 @@
 function getDefaultTheme() {
-  let hour = new Date().getHours();
-  return hour < 8 || hour > 18;
+  // let hour = new Date().getHours();
+  // return hour < 8 || hour > 18;
+
+  let d = new Date();
+  let offset = (new Date().getTimezoneOffset() / 60) * -1;
+  let n = new Date(d.getTime() + offset);
+  return n.getHours() < 8 || n.getHours() > 18;
 }
 
 export default getDefaultTheme;
